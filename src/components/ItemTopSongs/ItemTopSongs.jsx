e54eb784cc5b531625f0a1b1e76762963d8a069c
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-09-01 21:11:33 +0300

    added audio tag

--- src/components/ItemTopSongs/ItemTopSongs.jsx
+++ src/components/ItemTopSongs/ItemTopSongs.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const ItemTopSongs = () => {
-  return <div>ItemTopSongs</div>;
+const ItemTopSongs = ({ song }) => {
+  console.log(song.attributes.previews[0].url);
+  return (
+    <audio controls>
+      <source src={song.attributes.previews[0].url} />
+    </audio>
+  );
 };
 
 export default ItemTopSongs;
